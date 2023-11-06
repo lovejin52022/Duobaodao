74d41f9f44f2a19fe5496e22be8baeca39624649
zengjin <330913734@qq.com>
2023-11-06 09:37:00 +0800

    feat： jd duobaodao shop

---
+++ server/api.js
@@ -0,0 +1,13 @@
+/**
+ * 会用到的京东接口地址
+ * */
+const API = {
+	api_jd_hostname: "api.m.jd.com",
+	item_result: "https://1paipai.jd.com/auction-detail/",
+	api_jd_path: "/api",
+	api_jd: "https://api.m.jd.com/api",
+	item_url: "https://1paipai.jd.com/auction-detail/",
+	login_url: "https://passport.jd.com/new/login.aspx?ReturnUrl=https://1paipai.jd.com/auction-list",
+	login_success_redirect_url: "https://1paipai.jd.com/auction-list"
+};
+module.exports = API;
